76096ee8072751d209c5ab961a3e9625ffa6edde
Masoud Ranjbar Evaki <mranjbarev@gmail.com>
2020-04-25 18:21:53 +0430

    Initial commit

---
+++ app/src/main/java/ir/evaki/accounting/MainActivity.kt
@@ -0,0 +1,12 @@
+package ir.evaki.accounting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
